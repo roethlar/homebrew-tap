21caa20369a5f3a0c85ecc5106e5b72c608c0e80
Songr Release <release@songr.invalid>
2026-08-27 15:16:05 +0000

    songr 1.2.0

--- Casks/songr.rb
+++ Casks/songr.rb
@@ -1,9 +1,9 @@
 cask "songr" do
   arch arm: "-arm64"
 
-  version "1.1.6"
-  sha256 arm:   "4b72f2fde03bbe42da9e4ad145c4aaa9723be32db3249216a1cf61462967c9ff",
-         intel: "7f5e8e7ac1a38926fbe797d6d38d6c854f45dcf02ef0e74a3c0792d6b1133f87"
+  version "1.2.0"
+  sha256 arm:   "b08354491d8c4ce9c526b9ad8830f99ef9960fa8717059b306c33e63227ff075",
+         intel: "b3a9449f0a16dc2e1586d2e7f5daf3fc0a99cbcfe9ab1c5336d6425c7e303b5b"
 
   url "https://github.com/roethlar/songr/releases/download/v#{version}/Songr-#{version}#{arch}.dmg"
   name "Songr"
